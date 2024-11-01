b7a040eb11234e9082996432be33cb76b19775c9
Codey Gallup <codey.gallup@gmail.com>
2024-11-01 10:03:52 -0600

    Completed User class

--- src/User.js
+++ src/User.js
@@ -1,5 +1,21 @@
 class User {
-  // User code here
+  constructor(username, password, age) {
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;
+  }
+  login(password) {
+    if (this.password === password) {
+      this.loggedIn = true;
+      return true;
+    }
+    throw new Error("incorrect password");
+  }
+  logout() {
+    this.loggedIn = false;
+    return true;
+  }
 }
 
-module.exports = User
+module.exports = User;
